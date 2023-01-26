23ea927f31ef655d3a2461f46f6f880b6cec8df1
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-26 22:05:52 +0530

    rotation game

---
+++ Q4. Rotation Game.py
@@ -0,0 +1,13 @@
+def main():
+    a = list(map(int,input().split()))
+    N = a[0]
+    B = int(input())
+    A = a[1:]
+    B = B%N #normalize the B
+    rotated_A = A[-B:]+A[:-B]
+    for a in rotated_A:
+        print(a, end=" ")
+    return 0
+
+if __name__ == '__main__':
+    main()
