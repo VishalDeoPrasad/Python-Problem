67f0606f45ce65ced70596b978d352947f71070d
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-29 23:57:55 +0530

    python

--- Q4. Rotation Game.py
+++ Q4. Rotation Game.py
@@ -2,7 +2,7 @@ def main():
     a = list(map(int,input().split()))
     N = a[0]
     B = int(input())
-    A = a[1:]
+    A = a[1:] 
     B = B%N #normalize the B
     rotated_A = A[-B:]+A[:-B]
     for a in rotated_A:
