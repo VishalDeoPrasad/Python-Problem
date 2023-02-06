33b8fb6c1f98b5b83ca120dfa7ea9285d0125846
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-02-06 19:46:14 +0530

    Noble Integer problem

--- Noble Integer.py
+++ Noble Integer.py
@@ -1,5 +1,6 @@
 class Solution:
-    def solve(self, A):
+    # solve in O(n2)
+    def solve1(self, A):
         for i in range(len(A)):
             cnt = 0
             for j in range(len(A)):
@@ -7,6 +8,21 @@ class Solution:
                     cnt += 1    
             if cnt == A[i]:
                 return 1
+        return -1
+
+    #solve in O(n)
+    def solve(self, A):
+        A.sort()
+        n = len(A)
+        for i in range(n - 1):
+            if A[i] == A[i + 1]:
+                continue #skip the next element
+            if A[i] == n - i - 1: #how many element after this element
+            #if A[i] == len(A[i+1:]):
+                return 1
+        
+        if A[len(A) - 1] == 0: #if last element is 0 then return 1
+            return 1
         return -1
 
 print(Solution().solve([5,6,2]))
